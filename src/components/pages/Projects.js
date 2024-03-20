e114c18f1b5c36aae0d943340852dcaf217e2d2d
rachelrook <rrukavina913@gmail.com>
2024-03-20 11:33:34 -0500

    added ruby ruth to projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -6,6 +6,9 @@ export default function Projects() {
     return <><br></br><br></br><div className='center'>
       <Link to="https://franks-farms.com">
         <img className="i-size" src='/images/ff-thumb.jpg' alt="Franks Farms"/>
+        </Link><br></br><br></br>
+      <Link to="https://rubiesoftruth.com">
+        <img className="i-size" src='/images/rubyruth.jpg' alt="Rubies of Truth"/>
         </Link>
       </div></>
 }
