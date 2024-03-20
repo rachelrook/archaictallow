f1715ee508b009c9924b3bc1810976152244887d
rachelrook <rrukavina913@gmail.com>
2024-03-20 08:49:47 -0500

    added Franks Farms to projects page

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import '../../App.css';
 
 export default function Projects() {
-    return <h1 className="ph-text">Projects showcase coming soon!</h1>;
+    return <><br></br><br></br><div className='center'>
+      <Link to="https://franks-farms.com">
+        <img src='/images/ff-thumb.jpg' alt="Franks Farms"/>
+        </Link>
+      </div></>
 }
