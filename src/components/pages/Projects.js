33b1f73ec6776149af11a671cc450aac967e85db
rachelrook <rrukavina913@gmail.com>
2025-06-24 12:06:54 -0500

    test commit

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,6 +4,6 @@ import '../../App.css';
 
 export default function Projects() {
     return <><br></br><br></br><div className='center'>
-    <h1>Coming Soon!</h1>
+    <h1>Coming Soon! test</h1>
       </div></>
 }
