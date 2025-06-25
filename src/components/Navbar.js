05b7f3c414e0d314637446b6c1e8f610cd2c2547
rachelrook <rrukavina913@gmail.com>
2025-06-24 20:50:00 -0500

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,18 @@ function Navbar() {
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
+  
+  const showButton = () => {
+    if(window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true);
+    }
+    };
+  
+    useEffect(() => {
+      showButton();
+    }, []);
     
     window.addEventListener('resize', showButton);
   return (
